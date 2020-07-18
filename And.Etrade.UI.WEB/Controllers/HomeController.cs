3905180c2b9a21c763c45bf03a2ab904cd987b0d
OmerEkinci1 <omrekinci5@gmail.com>
2020-07-18 23:52:13 +0300

    start to build admin panel

---
+++ And.Etrade.UI.WEB/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using And.Etrade.Core.Model;
+using And.Etrade.Core.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace And.Etrade.UI.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        AndDB db = new AndDB();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.Products.OrderByDescending(x => x.CreateDate).Take(5).ToList();
+            return View(data);
+        }
+
+        public PartialViewResult GetMenu()
+        {         
+            var menus = db.Categories.Where(x => x.ParentID == 0).ToList();
+            return PartialView(menus);
+        }
+        [Route("Member-Login")]
+        public ActionResult Login()
+        {          
+            return View();
+        }
+        [HttpPost]
+        [Route("Member-Login")]
+        public ActionResult Login(string Email,string Password)
+        {
+            var users = db.Users.Where(x => x.Email == Email && x.Password == Password && x.IsActive == true && x.IsAdmin == false).ToList();
+            if (users.Count==1)
+            {
+                Session["LoginUser"] = users.FirstOrDefault();
+                return Redirect("/");
+            }
+            else
+            {
+                ViewBag.Error = "Wrong User or Password";
+                return View();
+            }   
+        }
+        [Route("Member-Register")]
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("Member-Register")]
+        public ActionResult CreateUser(User entity)
+        {
+            try
+            {
+                entity.CreateDate = DateTime.Now;
+                entity.CreateUserID = 1;
+                entity.IsActive = true;
+                entity.IsAdmin = false;
+
+                db.Users.Add(entity);
+                db.SaveChanges();
+                return Redirect("/");
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+            
+
+            
+        }
+    }
+}
