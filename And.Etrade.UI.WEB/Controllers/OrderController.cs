85d2a17cb41e7f73c3e187f5d181a2bca98913a1
OmerEkinci1 <omrekinci5@gmail.com>
2020-07-23 17:39:17 +0300

    Create Order Section and Manage Address Section

---
+++ And.Etrade.UI.WEB/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using And.Etrade.Core.Model;
+using And.Etrade.Core.Model.Entity;
+using And.Etrade.UI.WEB.Controllers.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace And.Etrade.UI.WEB.Controllers
+{
+    public class OrderController : AndControllerBase
+    {
+        // GET: Order
+        [Route("Giveorder")]
+        public ActionResult AddressList()
+        {
+            var db = new AndDB();
+            var data = db.Addresses.Where(x => x.UserID == LoginUserID).ToList();
+            return View(data);
+        }
+
+        public ActionResult CreateUserAddress(UserAddress entity)
+        {
+            entity.CreateDate = DateTime.Now;
+            entity.CreateUserID = LoginUserID;
+            entity.IsActive = true;
+            entity.UserID = LoginUserID;
+
+            var db = new AndDB();
+            db.Addresses.Add(entity);
+            db.SaveChanges();
+            return RedirectToAction("AddressList");
+        }
+    }
+}
